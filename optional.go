25f988858ed9643fcdf009e2a7c653b5fab9a37f
WinPooh32 <winpooh32@yandex.ru>
2025-01-25 15:02:17 +1000

    Add Less and Compare funcs

--- optional.go
+++ optional.go
@@ -25,4 +25,20 @@ func Wrap[U any](value U) T[U] {
 func Unwrap[U any](x T[U]) (U, bool) {
 	return x.value, x.set
 }
+
+func Less[U cmp.Ordered](x, y T[U]) bool {
+	return (!x.set && y.set) || cmp.Less(x.value, y.value)
+}
+
+func Compare[U cmp.Ordered](x, y T[U]) int {
+	if !x.set && !y.set {
+		return 0
+	}
+	if x.set && !y.set {
+		return 1
+	}
+	if !x.set && y.set {
+		return -1
+	}
+	return cmp.Compare(x.value, y.value)
 }

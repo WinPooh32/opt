0f1de13c4518dcfdf8c1f7c57ba2496d22d363f5
WinPooh32 <winpooh32@yandex.ru>
2025-01-28 03:10:01 +1000

    Add Compare test cases

--- optional_test.go
+++ optional_test.go
@@ -46,11 +46,38 @@ func TestLess(t *testing.T) {
 		{"greater", args{opt.Wrap(9.0), opt.Wrap(4.0)}, false},
 		{"x is NaN", args{opt.Wrap(math.NaN()), opt.Wrap(4.0)}, true},
 		{"y is NaN", args{opt.Wrap(4.0), opt.Wrap(math.NaN())}, false},
+		{"x, y both are NaN", args{opt.Wrap(math.NaN()), opt.Wrap(math.NaN())}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := opt.Less(tt.args.x, tt.args.y); got != tt.want {
 				t.Errorf("Less() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompare(t *testing.T) {
+	type args struct {
+		x opt.T[float64]
+		y opt.T[float64]
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"less", args{opt.Wrap(4.0), opt.Wrap(9.0)}, -1},
+		{"equal", args{opt.Wrap(4.0), opt.Wrap(4.0)}, 0},
+		{"greater", args{opt.Wrap(9.0), opt.Wrap(4.0)}, 1},
+		{"x is NaN", args{opt.Wrap(math.NaN()), opt.Wrap(4.0)}, -1},
+		{"y is NaN", args{opt.Wrap(4.0), opt.Wrap(math.NaN())}, 1},
+		{"x, y both are NaN", args{opt.Wrap(math.NaN()), opt.Wrap(math.NaN())}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := opt.Compare(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Compare() = %v, want %v", got, tt.want)
 			}
 		})
 	}
